65ae56da348f2c83fb839968fd1c00cf9b9afdae
fadlan <fadlananshari06@gmail.com>
2023-10-18 16:13:14 +0700

    fix

--- src/pages/Artikel.jsx
+++ src/pages/Artikel.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Article_img from '../assets/article-img.png';
 import Arrow_icon from '../assets/arrow-icon.png';
 import Comment_icon from '../assets/comment-icon.png';
-import Love_icon from '../assets/Love-icon.png';
-import Eye_icon from '../assets/Eye-icon.png';
+import Love_icon from '../assets/love-icon.png';
+import Eye_icon from '../assets/eye-icon.png';
 import Search from '../assets/search.svg';
 
 function Artikel() {
@@ -183,7 +183,7 @@ function Artikel() {
               </div>
             </div>
           </div>
-          <div className='text-end'>
+          <div className="text-end">
             <a href="" className="btn btn-white fw-bold">
               Selanjutnya &#10140;
             </a>
